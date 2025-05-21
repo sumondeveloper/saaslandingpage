cd1fb2816965df7de721980284b2c39e6ca14ccc
Rakibul Islam Sumon <rakibuldevs@gmail.com>
2025-05-21 09:57:59 +0600

    add logoticker animation to the website

--- components/sections/LogoTicker.tsx
+++ components/sections/LogoTicker.tsx
@@ -1,6 +1,7 @@
 import { LogoItems } from "@/lib/constant";
 import MaxWidthWrapper from "../shared/MaxWidthWrapper";
 import Image from "next/image";
+import * as motion from "motion/react-client";
 
 const LogoTicker = () => {
   return (
@@ -10,8 +11,13 @@ const LogoTicker = () => {
           Trusted by the world’s most innovative teams
         </h2>
 
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:to-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
-          <div className="flex gap-16 justify-center">
+        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:z-10 before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:to-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <motion.div
+            initial={{ translateX: 0 }}
+            animate={{ translateX: "-50%" }}
+            transition={{ duration: 10, ease: "linear", repeat: Infinity }}
+            className="flex gap-16 justify-center pr-16"
+          >
             {LogoItems.map((item, index) => (
               <Image
                 src={item}
@@ -20,7 +26,15 @@ const LogoTicker = () => {
                 className="h-8 w-auto flex-none"
               />
             ))}
-          </div>
+            {LogoItems.map((item, index) => (
+              <Image
+                src={item}
+                alt=""
+                key={index * 5}
+                className="h-8 w-auto flex-none"
+              />
+            ))}
+          </motion.div>
         </div>
       </MaxWidthWrapper>
     </div>
